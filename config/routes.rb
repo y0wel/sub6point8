f025138edf46efdcc4a8185455264beb0d023af6
Tobias <matz_tobias@web.de>
2024-12-16 16:21:14 +0100

    setup routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :user, controllers: {
+    confirmations: 'user/confirmations',
+    passwords:     'user/passwords',
+    sessions:      'user/sessions',
+    unlocks:       'user/unlocks',
+    registrations: 'user/registrations'
+  }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -10,5 +16,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "dashboard#index", as: :dashboard
 end
