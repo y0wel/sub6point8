1cf857e12168cea68ce79d1782b98d568f804b44
Tobias <matz_tobias@web.de>
2024-12-20 13:44:47 +0100

    adjust tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/assets/stylesheets/**/*.css',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
       },
     },
   },
